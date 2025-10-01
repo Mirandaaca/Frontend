8d6acc051d7d28b084389e23d7bde12896628488
Cristopher Miranda Aramayo <113213195+Mirandaaca@users.noreply.github.com>
2025-10-01 15:23:45 -0400

    Update userService.js
    Added a new method to userService

--- src/services/userService.js
+++ src/services/userService.js
@@ -48,4 +48,24 @@ export const updateUser = async (form)=>{
         throw new Error("error fetching")
     }
 
+
+}
+
+export const deleteUser = async (form)=>{
+
+    try {
+        const response = await fetch(`${API_URL}/users/${form.id}`, {
+            method: "PUT", 
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(form),
+        })
+        if(!response.ok) throw new Error("error fetching")
+        return await response.json()
+    } catch (error) {
+        throw new Error("error fetching")
+    }
+
+
 }
